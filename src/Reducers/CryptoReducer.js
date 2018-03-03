a7db4b4ea0c49f3ca9b05750fae7f8fc5a3bb396
Tyler Bussanich <tbussanich@gmail.com>
2018-03-02 16:22:43 -0800

    implement second and thirs reducers

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -21,6 +21,24 @@ export default function(state = initialState, action) {
         errorMessage: null
       });
 
+    case FETCHING_COIN_DATA_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: false,
+        errorMessage: null
+      });
+
+    case FETCHING_COIN_DATA_FAIL:
+      return Object.assign({}, state, {
+        isFetching: false,
+        data: action.payload,
+        hasError: true,
+        errorMessage: action.err
+      });
+
+
+
       default:
         return state;
   }
