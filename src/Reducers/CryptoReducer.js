03a9d653872668c176dd8b40705b2b3879798924
Tyler Bussanich <tbussanich@gmail.com>
2018-03-02 16:17:56 -0800

    implement first reducer

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,27 @@
-export default function(state = [], action) {
-  return state;
+import {
+  FETCHING_COIN_DATA,
+  FETCHING_COIN_DATA_FAIL,
+  FETCHING_COIN_DATA_SUCCESS
+} from './../Utils/ActionTypes';
+
+const initialState = {
+  isFetching: null,
+  data: [],
+  hasError: false,
+  errorMessage: null,
+};
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case FETCHING_COIN_DATA:
+      return Object.assign({}, state, {
+        isFetching: true,
+        data: null,
+        hasError: false,
+        errorMessage: null
+      });
+
+      default:
+        return state;
+  }
 }
