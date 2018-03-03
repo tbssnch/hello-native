9809dc76f3720180eeee923a6de1120813469844
Tyler Bussanich <tbussanich@gmail.com>
2018-03-02 16:10:50 -0800

    add actions and action types

---
+++ src/Actions/FetchCoinData.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { apiBaseURL } from './../utils/Constants';
+
+import {
+  FETCHING_COIN_DATA,
+  FETCHING_COIN_DATA_FAIL,
+  FETCHING_COIN_DATA_SUCCESS
+} from './../Utils/ActionTypes';
+
+export default function FetchCoinData() {
+  return dispatch => {
+    dispatch({
+      type: FETCHING_COIN_DATA
+    })
+
+    return axios.get(`${apiBaseURL}/v1/ticker/?limit=10`)
+      .then(res => {
+        dispatch({
+          type: FETCHING_COIN_DATA_SUCCESS,
+          payload: res.data
+        })
+      .catch(err => {
+        dispatch({
+          type: FETCHING_COIN_DATA_FAIL,
+          payload: err.data
+        })
+      })
+    });
+  }
+}
---
+++ src/Utils/Constants.js
@@ -0,0 +1,1 @@
+export const apiBaseURL = 'http://api.coinmarketcap.com';
